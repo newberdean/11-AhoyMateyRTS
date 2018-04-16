5ccef1f5a56e0bb08712b84f6f44f57fa65f6a48
Deano <8463497+newberdean@users.noreply.github.com>
2018-04-16 20:42:49 +1200

    First Commit

---
+++ 11-AhoyMateyRTS/Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+	public static NetworkClient hostClient;
+
+	public void MyStartHost (){
+		Debug.Log (Time.timeSinceLevelLoad + " - Host Starting");
+		hostClient = StartHost ();
+	}
+
+	public override void OnStartHost(){
+		Debug.Log (Time.timeSinceLevelLoad + " - Host Started");
+		base.OnStartHost ();
+	}
+
+	public override void OnStartClient(NetworkClient myClient){
+		Debug.Log (Time.timeSinceLevelLoad + " - Client Start Requested");
+		base.OnStartClient (myClient);
+		InvokeRepeating("WaitingForConnection", 0f, 1f);
+	}
+
+	public override void OnClientConnect(NetworkConnection conn){
+		CancelInvoke ("WaitingForConnection");
+		Debug.Log (Time.timeSinceLevelLoad + " - Client Connected to " + conn.address);
+		base.OnClientConnect (conn);
+	}
+
+	void WaitingForConnection(){
+		Debug.Log (".");
+	}
+}
