7b573213c47ea30678126e095a68b145cf3d9d99
Deano <8463497+newberdean@users.noreply.github.com>
2018-04-17 19:08:30 +1200

    ... Aaaaaaaand I'm Done with the Video Lectures that pertain to helping me with this particular game.
    Now to make this toy into a game!

---
+++ 11-AhoyMateyRTS/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using USACPI = UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager;
+
+public class MovePlayer : NetworkBehaviour {
+
+	public float hSpeed = 3f, vSpeed = 3f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isLocalPlayer) {
+			return;
+		}
+		float hMove = USACPI.GetAxis ("Horizontal") * hSpeed;
+		float vMove = USACPI.GetAxis ("Vertical") * vSpeed;
+
+		transform.Translate (new Vector3 (hMove, 0f, vMove));
+	}
+}
---
+++ 11-AhoyMateyRTS/Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Player : NetworkBehaviour {
+
+	public Material myMaterial;		// Material I see.
+	public Material notMyMaterial;	// Everyone else.
+	public string myName;			// Name above my head.
+
+	Canvas canvas;
+	Text myNameText;
+	Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		canvas = GetComponentInChildren<Canvas> ();
+		myNameText = GetComponentInChildren<Text> ();
+		myNameText.text = myName;
+		Vector2 size = myNameText.GetComponent<RectTransform> ().sizeDelta;
+		size.x = myNameText.preferredWidth;
+		size.y = myNameText.preferredHeight;
+		canvas.GetComponent<RectTransform> ().sizeDelta = size;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!cam)
+			cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+		if (myNameText.text != myName && isLocalPlayer) {
+			myNameText.text = myName;
+			Vector2 size = myNameText.GetComponent<RectTransform> ().sizeDelta;
+			size.x = myNameText.preferredWidth;
+			size.y = myNameText.preferredHeight;
+			canvas.GetComponent<RectTransform> ().sizeDelta = size;
+		}
+		canvas.transform.LookAt (cam.transform);
+		canvas.transform.RotateAround (canvas.transform.position, canvas.transform.up, 180f);
+	}
+
+	public override void OnStartLocalPlayer(){
+		transform.GetChild (1).gameObject.SetActive (true);
+		GetComponent<MovePlayer> ().enabled = true;
+		GetComponent<MeshRenderer> ().material = myMaterial;
+		base.OnStartLocalPlayer ();
+	}
+}
